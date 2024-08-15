36c3eff5e6559ea843668ae0e71e9c4e2c5ec482
Cleny Nguyễn <kushirotm@gmail.com>
2024-08-15 16:00:15 +0700

    add subtype

--- GemidexAPI/Model/Dto/GemiObjectModel.cs
+++ GemidexAPI/Model/Dto/GemiObjectModel.cs
@@ -4,6 +4,7 @@
     {
         public string GoogleId { get; set; }
         public string TypeName { get; set; }
+        public string? SubTypeName { get; set; }
         public string GemiName { get; set; }
         public string Description { get; set; }
         public string Weight { get; set; }
